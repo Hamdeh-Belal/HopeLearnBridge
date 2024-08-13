9960a81ccef0949815cbd05801dc1e08a57ccb8e
T-Tasneem Ghazal <tghazal@asaltech.com>
2024-08-13 11:48:57 +0000

    Merged PR 62: Add react router
    Related work items: #138

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
