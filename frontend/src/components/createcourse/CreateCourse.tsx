87a8e72fd787461ea38a0f44b66267f4d3c3d248
T-Tasneem Ghazal <tghazal@asaltech.com>
2024-09-01 11:28:12 +0000

    Merged PR 78: Create course api using react query library
    Add tanstack react query library and using it to handle data fetching and updating
    
    Related work items: #153

--- frontend/src/components/createcourse/CreateCourse.tsx
+++ frontend/src/components/createcourse/CreateCourse.tsx
@@ -1,5 +1,4 @@
 import { Input } from 'antd';
-import { useForm } from 'react-hook-form';
 import {
   ButtonContainer,
   CreateCourseContainer,
@@ -8,15 +7,32 @@ import {
   StyledButton,
   StyledForm,
 } from './CreateCourse.style';
-import {CREATE_COURSE_ID } from './CreateCourse.const';
+import { CourseFormData, CREATE_COURSE_ID } from './CreateCourse.const';
+import { useCreateCourse } from '../../hooks/mutations/useCreateCourse/useCreateCourse';
+import { useForm } from 'react-hook-form';
+import LoadingScreen from '../loadingScreen';
+import LoadingError from '../loadingerror';
 
 const CreateCourse = () => {
-  const { register} = useForm();
+  const { register,handleSubmit } = useForm<CourseFormData>();
+  const { mutate, isPending, isError, errorMessage, reset } = useCreateCourse();
+
+  const onSubmit = (data: CourseFormData) => {
+    mutate(data);
+  };
+
+  if (isPending) {
+    return <LoadingScreen />;
+  }
+
+  if (isError) {
+    return <LoadingError errorMessage={errorMessage || 'An unexpected error occurred.'} onRetry={reset} />;
+  }
 
   return (
     <CreateCourseContainer data-testid={CREATE_COURSE_ID}>
       <CreateCourseHeader>Create a New Course!</CreateCourseHeader>
-      <StyledForm layout="vertical">
+      <StyledForm layout="vertical" onFinish={handleSubmit(onSubmit)}>
         <Label name="title" label="Title:">
           <Input placeholder="Title" {...register('title')} />
         </Label>
@@ -28,7 +44,7 @@ const CreateCourse = () => {
           />
         </Label>
         <ButtonContainer>
-          <StyledButton type="primary" htmlType="submit">
+          <StyledButton type="primary" htmlType="submit" loading={isPending}>
             Create!
           </StyledButton>
         </ButtonContainer>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,20 +8,25 @@ import Home from './pages/home';
 import Teacher from './pages/teacher';
 import ViewCourses from './pages/viewcourses';
 import CreateCourse from './components/createcourse';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/teacher" element={<Teacher />}>
-            <Route index element={<ViewCourses />} />
-            <Route path="create-course" element={<CreateCourse />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/teacher" element={<Teacher />}>
+              <Route index element={<ViewCourses />} />
+              <Route path="create-course" element={<CreateCourse />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
